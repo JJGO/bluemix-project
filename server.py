e665189e42b4e848637f377f888033ac74d89a07
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-05-21 18:30:05 +0200

    Working example of analyze_text

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request, jsonify, session
-import cf_deployment_tracker
+import datetime
 import atexit
 import os
 import random
-from services import load_credentials, get_database, get_watson_service, teardown_databases
+import shutil
+import string
 import sys
-import datetime
+
+import cf_deployment_tracker
+
+from flask import Flask, render_template, request, jsonify, session, url_for
+from services import load_credentials, get_database, get_watson_service, teardown_databases, get_speech_voices
+import watson_developer_cloud.natural_language_understanding.features.v1 as features
+
 
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
@@ -13,6 +19,7 @@ cf_deployment_tracker.track()
 app = Flask(__name__)
 
 app.config['DATABASE'] = 'mydb'
+app.config['TMPFOLDER'] = 'tmp'
 app.secret_key = "\xfd4\xadtJ\x1a'\xed\xe9\x0e`{\xd4\x8a\x11.ah\x87j\t\xad\x9e\xac"
 
 vcap = load_credentials()
@@ -53,6 +60,39 @@ def recent_searches():
     sorted_searches = [name for (name, timestamp) in sorted(past_searches, key=lambda x: x[1], reverse=True)]
 
     return jsonify(sorted_searches)
+
+
+@app.route('/api/analyze-text', methods=['POST', 'GET'])
+def analyze_text():
+    # text = request.json['text']
+    text = "No pienso comer ensalada, es un plato asqueroso e insípido"
+    translator = get_watson_service('language_translator')
+    nlu = get_watson_service('natural-language-understanding')
+    text_to_speech = get_watson_service('text_to_speech')
+    voices = get_speech_voices()
+
+    source_lang = translator.identify(text)['languages'][0]['language']
+    english_text = translator.translate(text, source=source_lang, target='en')
+
+    audioEN = text_to_speech.synthesize(english_text, accept='audio/ogg', voice="en-GB_KateVoice")
+
+    audiofile = ''.join(random.choice(string.ascii_lowercase) for i in range(20)) + '.ogg'
+    audiofile = os.path.join(app.config['TMPFOLDER'], audiofile)
+
+    with open(os.path.join('static', audiofile), 'wb') as f:
+        f.write(audioEN)
+
+    emotions = nlu.analyze(text=english_text, features=[features.Emotion()])
+    emotions = emotions['emotion']['document']['emotion']
+
+    audiourl = url_for('static', filename=audiofile)
+
+    s = render_template('output-text.html', audiourl=audiourl, emotions=emotions)
+
+    with open('test.html', 'w') as f:
+        print(s, file=f)
+
+    return s
 
 
 @app.route('/api/visitors', methods=['GET'])
@@ -105,5 +145,10 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if '--debug' in sys.argv:
             debug = True
+
+    tmpfolder = os.path.join('static', app.config['TMPFOLDER'])
+    if os.path.isdir(tmpfolder):
+        shutil.rmtree(tmpfolder)
+    os.mkdir(tmpfolder)
 
     app.run(host='0.0.0.0', port=port, debug=debug)
