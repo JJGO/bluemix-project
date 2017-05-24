0dc1f5415e9dbbe83cc9d9a7208d2662ac4e1bd1
Lucia <lumosan02@gmail.com>
2017-05-24 14:07:19 +0200

    Refactor JS for history and make history template

--- static/js/script-main.js
+++ static/js/script-main.js
@@ -5,19 +5,7 @@
       var text = $('#input_frase_arg').val();
         if (e.which == 13 && text.length > 0) { //catch Enter key
           //POST request to API to create a new visitor entry in the database
-            $.ajax({
-      method: "POST",
-      url: "./api/analyze-text",
-      contentType: "application/json",
-      data: JSON.stringify({text: text })
-    })
-            .done(function(data) {
-                $('#response').html(data);
-                $('#input_frase').hide();
-                $('#banner-fig').hide();
-                $('#input_imagen_all').hide();
-                //getNames();
-            });
+            analyze_text(text);
         }
     });
 
@@ -26,49 +14,64 @@
       var text = $('#input_imagen_arg').val();
         if (e.which == 13 && text.length > 0) { //catch Enter key
           //POST request to API to create a new visitor entry in the database
-            $.ajax({
-      method: "POST",
-      url: "./api/analyze-image",
-      contentType: "application/json",
-      data: JSON.stringify({text: text })
-    })
-            .done(function(data) {
-                $('#response').html(data);
-                $('#input_imagen').hide();
-                $('#banner-fig').hide();
-                $('#input_frase_all').hide();
-                //getNames();
-            });
+            analyze_image(text);
         }
     });
 
-    //Retrieve all the visitors from the database
-    //function getNames(){
-    //  $.get("./api/visitors")
-    //      .done(function(data) {
-    //          if(data.length > 0) {
-    //            $('#databaseNames').html("Database contents: " + JSON.stringify(data));
-    //          }
-    //      });
-    //  }
-
-      //Call getNames on page load.
-    //  getNames();
-
-
-
-
-
-
+    function analyze_text(text){
+      $.ajax({
+          method: "POST",
+          url: "./api/analyze-text",
+          contentType: "application/json",
+          data: JSON.stringify({text: text })
+        })
+      .done(function(data) {
+          // $('#input_frase').hide();
+          // $('#input_imagen_all').hide();
+          $('#banner-fig').hide();
+          $('#input_frase_arg').val("");
+          $('#response').html(data);
+          getHistory();
+      });
+    };
 
 
 
+    function analyze_image(text){
 
+      $.ajax({
+        method: "POST",
+        url: "./api/analyze-image",
+        contentType: "application/json",
+        data: JSON.stringify({text: text })
+      })
+      .done(function(data) {
+          // $('#input_imagen').hide();
+          // $('#input_frase_all').hide();
+          $('#banner-fig').hide();
+          $('#input_imagen_arg').val("");
+          $('#response').html(data);
+          getHistory();
+      });
+    };
+
+    //Retreive all the visitors from the database
+    function getHistory(){
+      $.get("./api/recent")
+          .done(function(data) {
+              if(data.length > 0) {
+                $('#resultado_historial').html(data);
+              }
+          });
+      }
+
+      //Call getHistory on page load.
+      getHistory();
 
 
 $body = $("body");
 
 $(document).on({
     ajaxStart: function() { $body.addClass("loading");    },
-     ajaxStop: function() { $body.removeClass("loading"); }    
+     ajaxStop: function() { $body.removeClass("loading"); }
 });
